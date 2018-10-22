0b83b41aac9cd50933c51dbba8946597d0e824ff
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2018-10-21 19:17:01 -0700

    Initial commit
    Signed-off-by: Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>

---
+++ pipeline/download.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# Download telemetry for satellites.  Starting with Elfin-A, as there
+# are grafana dashboards that can be used to verify I've gotten things
+# right: https://dashboard-dev.satnogs.org/d/-9-XUA0mz/elfin-a?orgId=1
