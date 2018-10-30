86f628a6800641fd5d199cc0adf4aa5ac9771d53
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2018-10-30 19:07:09 +0100

    Move notes to notebook

--- pipeline/download.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-
-# Download telemetry for satellites.  Starting with Elfin-A, as there
-# are grafana dashboards that can be used to verify I've gotten things
-# right: https://dashboard-dev.satnogs.org/d/-9-XUA0mz/elfin-a?orgId=1
--- pipeline/parse_csv.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-# Parse telemetry for satellites.  Starting with Elfin-A, as there
-# are grafana dashboards that can be used to verify I've gotten things
-# right: https://dashboard-dev.satnogs.org/d/-9-XUA0mz/elfin-a?orgId=1
-#
-# DB page: https://db.satnogs.org/satellite/43617/
