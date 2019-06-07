57ea9f5b0853361132376e91609b0f24984e3e07
Aditya Malshikhare <malshikhareaditya@gmail.com>
2019-06-07 09:28:55 +0000

    Update data_fetch_decoder.py

--- polaris/data_fetch/data_fetch_decoder.py
+++ polaris/data_fetch/data_fetch_decoder.py
@@ -1,8 +1,15 @@
 import os
-
+import argparse
+class Fetch(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        data_fetch_decode()
 def data_fetch_decode():
-    norad=input('Enter the NORAD ID of the satellite: ')
-    start=input('Enter the start timestamp (YYYY-MM-DDTHH:MM:SS): ')
-    end= input('Enter the end timestamp (YYYY-MM-DDTHH:MM:SS: ')
-    cmd='cd ../utils/glouton/; python3 ./glouton.py -s'+start+' -e '+end+' -n '+norad+' --demoddata --demodm CSV'
-    os.system(cmd)
+    fetch_cmd='cd ../utils/glouton-satnogs-data-downloader/; python3 ./glouton.py -s 2019-05-01T00:00:00 -e 2019-06-01T00:00:00 -n 43617 --demoddata --demodm CSV'
+    os.system(fetch_cmd)
+    all_directories = [d for d in os.listdir('.') if os.path.isdir(d)]
+    latest_directory = max(all_directories, key = os.path.getatime)
+    print('Saving the dataframes in directory: '+latest_directory)
+    merge_cmd='cd ../utils/glouton-satnogs-data-downloader/'+latest_directory+'; sed 1d *.csv > ../merged_frames.csv'
+    os.system(merge_cmd)
+    decode_cmd='cd ../utils/satnogs-decoders/; python decode-multiple.py -d Elfin -p -x ../glouton-satnogs-data-downloader/merged_frames.csv > ../../decoded_frames.json'
+    os.system(decode_cmd)
