0dc3de632932be6aa39a0290210906ca54412dd2
Aditya Malshikhare <malshikhareaditya@gmail.com>
2019-06-13 14:59:12 +0000

    Update data_fetch_decoder.py

--- polaris/data_fetch/data_fetch_decoder.py
+++ polaris/data_fetch/data_fetch_decoder.py
@@ -1,15 +1,39 @@
+import subprocess
 import os
 import argparse
+
+
 class Fetch(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         data_fetch_decode()
-def data_fetch_decode():
-    fetch_cmd='cd ../utils/glouton-satnogs-data-downloader/; python3 ./glouton.py -s 2019-05-01T00:00:00 -e 2019-06-01T00:00:00 -n 43617 --demoddata --demodm CSV'
-    os.system(fetch_cmd)
+
+
+def get_output_directory():
     all_directories = [d for d in os.listdir('.') if os.path.isdir(d)]
-    latest_directory = max(all_directories, key = os.path.getatime)
-    print('Saving the dataframes in directory: '+latest_directory)
-    merge_cmd='cd ../utils/glouton-satnogs-data-downloader/'+latest_directory+'; sed 1d *.csv > ../merged_frames.csv'
-    os.system(merge_cmd)
-    decode_cmd='cd ../utils/satnogs-decoders/; python decode-multiple.py -d Elfin -p -x ../glouton-satnogs-data-downloader/merged_frames.csv > ../../decoded_frames.json'
-    os.system(decode_cmd)
+    output_directory = max(all_directories, key=os.path.getmtime)
+    return output_directory
+
+
+def data_fetch_decode():
+    start_date = '2019-05-01T00:00:00'
+    end_date = '2019-05-10T00:00:00'
+    data_directory = '../data'
+    fetch_cmd = 'python3 ./glouton.py --wdir'+data_directory+' -s '+start_date+' -e '+end_date+' -n 43617 --demoddata --demodm CSV'
+    try:
+        subprocess.Popen(fetch_cmd, shell=True, cwd='../utils/glouton-satnogs-data-downloader')
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
+    output_directory = get_output_directory()
+    print('Saving the dataframes in directory: '+output_directory)
+    path_to_output_directory = data_directory+output_directory
+    merge_cmd = 'sed 1d *.csv > ../merged_frames.csv'
+    try:
+        subprocess.Popen(merge_cmd, shell=True, cwd=path_to_output_directory)
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
+    merged_file = 'merged_frames.csv'
+    decode_cmd = 'python decode-multiple.py -d Elfin -p -x ../../data/'+merged_file+' > ../../data/decoded_frames.json'
+    try:
+        subprocess.Popen(decode_cmd, shell=True, cwd='../utils/satnogs-decoders/')
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
