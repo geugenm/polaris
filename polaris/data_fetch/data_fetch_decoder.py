370adea89301e67df4ea9b9fbe3b71ccfdd09cb7
Aditya Malshikhare <malshikhareaditya@gmail.com>
2019-06-19 17:52:19 +0000

    Add function to truncate the first line(Input file: ) from the JSON output file and some minor changes.

--- polaris/data_fetch/data_fetch_decoder.py
+++ polaris/data_fetch/data_fetch_decoder.py
@@ -18,19 +18,31 @@ def get_output_directory(data_directory=DATA_DIRECTORY):
     return output_directory
 
 
+def build_truncate_first_line_cmd(src):
+    """ Build command to truncate the first line from the output JSON file
+    which is 'Input file' line resulting from decode_multiple script
+    from satnogs-decoders.
+    """
+    truncate_cmd = 'sed -i {src} -e \'1d;\''.format(
+        src=src,
+        )
+    return truncate_cmd
+
+
 def build_decode_cmd(src, dest):
     """Build command to decode downloaded into JSON
     """
     decode_multiple = 'decode_multiple'
     decoder_module = 'Elfin'
     input_format = 'csv'
-    decode_cmd = '{decode_multiple} --filename {src} --format {input_format} {decoder_module} > ../../{dest}'.format(
-        decode_multiple=decode_multiple,
-        decoder_module=decoder_module,
-        src='/tmp/polaris/'+src,
-        input_format=input_format,
-        dest=dest,
-    )
+    decode_cmd = '{decode_multiple} --filename {src} --format {input_format}'\
+        ' {decoder_module} > ../../{dest}'.format(
+                                    decode_multiple=decode_multiple,
+                                    decoder_module=decoder_module,
+                                    src='/tmp/polaris/'+src,
+                                    input_format=input_format,
+                                    dest=dest,
+                                    )
     return decode_cmd
 
 
@@ -38,19 +50,20 @@ def build_fetch_cmd():
     """Build command to fetch data from SatNOGS for a particular
     satellite
     """
-    start_date = '2019-05-01T00:00:00'  # Start timestamp
-    end_date = '2019-05-05T00:00:00'  # End timestamp
+    start_date = '2019-05-05T00:00:00'  # Start timestamp
+    end_date = '2019-05-10T00:00:00'  # End timestamp
     glouton = 'python3 ./glouton.py'
     demod_args = '--demoddata --demodm CSV'
     sat = '43617'  # Elfin-A
-    cmd = '{glouton} --wdir {data_dir} -s {start_date} -e {end_date} -n {sat} {demod_args}'.format(
-        glouton=glouton,
-        data_dir=DATA_DIRECTORY,
-        start_date=start_date,
-        end_date=end_date,
-        sat=sat,
-        demod_args=demod_args
-    )
+    cmd = '{glouton} --wdir {data_dir} -s {start_date} -e {end_date} -n'\
+        ' {sat} {demod_args}'.format(
+                    glouton=glouton,
+                    data_dir=DATA_DIRECTORY,
+                    start_date=start_date,
+                    end_date=end_date,
+                    sat=sat,
+                    demod_args=demod_args
+                    )
     return cmd
 
 
@@ -85,33 +98,34 @@ def data_fetch_decode():
                               shell=True,
                               cwd=path_to_output_directory)
         p2.wait()
+        print('Merge Completed')
+        print('Storing merged CSV file: '+merged_file)
     except subprocess.CalledProcessError as err:
         print('ERROR:', err)
-    print('Merge Completed')
-    print('Storing merged CSV file: '+merged_file)
     print('Starting decoding of the data')
     decoded_file = 'decoded_frames_'+output_directory+'.json'
     # Using satnogs-decoders to decode the CSV files containing
-    # multiple dataframes as a JSON objects.
-    # decode-multiple.py is not present in satnogs-decoders repository
-    # You can download the script from the code snippets
-    # [https://gitlab.com/librespacefoundation/satnogs/satnogs-decoders/snippets/1795023]
-    # Put that script in polaris/utils/satnogs-decoders and you're good to go!
+    # multiple dataframes and store them as JSON objects.
     decode_cmd = build_decode_cmd(merged_file, decoded_file)
     try:
         absolute_file_directory = os.path.dirname(os.path.abspath(__file__))
         os.chdir(absolute_file_directory)
-        # TODO: On the fence here about whether to use shell
-        # direction to create the output file, or to capture output &
-        # write to file ourselves.
         p3 = subprocess.Popen(decode_cmd,
                               shell=True,
                               cwd='../../utils/satnogs-decoders')
         p3.wait()
+        print('Decoding of data finished.')
     except subprocess.CalledProcessError as err:
         print('ERROR:', err)
-    print('Decoding of data finished.')
-    print(
-        'Storing the decoded data JSON file in root directory :'
-        + decoded_file
+    truncate_cmd = build_truncate_first_line_cmd(decoded_file)
+    try:
+        p4 = subprocess.Popen(truncate_cmd,
+                              shell=True,
+                              cwd='../../')
+        p4.wait()
+        print(
+            'Storing the decoded data JSON file in root directory :'
+            + decoded_file
         )
+    except subprocess.CalledProcessError as err:
+        print('ERROR:', err)
