bb8b1d54efff42fb3e495e2ef123b35e17c612ea
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2019-06-18 20:38:57 -0700

    Refactor these functions a bit

--- polaris/data_fetch/data_fetch_decoder.py
+++ polaris/data_fetch/data_fetch_decoder.py
@@ -24,16 +24,54 @@ def get_output_directory(data_directory=data_directory):
     return output_directory
 
 
-def data_fetch_decode():
+def build_decode_cmd(src, dest):
+    """Build command to decode downloaded into JSON
+    """
+    decode_multiple = 'python decode-multiple.py'
+    decoder_module = 'Elfin'
+    decode_cmd = '{decode_multiple} -d {decoder_module} -p -x {src} > ../../{dest}'.format(
+        decode_multiple=decode_multiple,
+        decoder_module=decoder_module,
+        src=src,
+        dest=dest,
+    )
+    return decode_cmd
+
+
+def build_fetch_cmd():
+    """Build command to fetch data from SatNOGS for a particular
+    satellite
+    """
     start_date = '2019-05-01T00:00:00'  # Start timestamp
     end_date = '2019-05-10T00:00:00'  # End timestamp
-    # Shell command for executing glouton in order to download the dataframes from SatNOGS network based on NORAD ID of the satellite and start and end timestamps
+    glouton = 'python3 ./glouton.py'
+    demod_args = '--demoddata --demodm CSV'
+    sat = '43617'  # Elfin-B
+    cmd = '{glouton} --wdir {data_dir} -s {start_date} -e {end_date} -n {sat} {demod_args}'.format(
+        glouton=glouton,
+        data_dir=data_directory,
+        start_date=start_date,
+        end_date=end_date,
+        sat=sat,
+        demod_args=demod_args
+    )
+    return cmd
+
+
+def data_fetch_decode():
+    """Main function to download and decode satellite telemetry"""
     if not os.path.exists(data_directory):
         os.makedirs(data_directory)
-    fetch_cmd = 'python3 ./glouton.py --wdir '+data_directory+' -s '+start_date+' -e '+end_date+' -n 43617 --demoddata --demodm CSV'
+    # Shell command for executing glouton in order to download the
+    # dataframes from SatNOGS network based on NORAD ID of the
+    # satellite and start and end timestamps
+    fetch_cmd = build_fetch_cmd()
     try:
-        # Using subprocess package to execute fetch command by passing current working directory as an argument
-        p1 = subprocess.Popen(fetch_cmd, shell=True, cwd='../utils/glouton-satnogs-data-downloader')
+        # Using subprocess package to execute fetch command by passing
+        # current working directory as an argument
+        p1 = subprocess.Popen(fetch_cmd,
+                              shell=True,
+                              cwd='../utils/glouton-satnogs-data-downloader')
         p1.wait()
     except subprocess.CalledProcessError as err:
         print('ERROR:', err)
@@ -62,10 +100,13 @@ def data_fetch_decode():
     # decode-multiple.py is not present in satnogs-decoders repository
     # You can download the script from the code snippets [https://gitlab.com/librespacefoundation/satnogs/satnogs-decoders/snippets/1795023]
     # Put that script in polaris/utils/satnogs-decoders and you're good to go!
-    decode_cmd = 'python decode-multiple.py -d Elfin -p -x /tmp/polaris/'+merged_file+' > ../../'+decoded_file
+    decode_cmd = build_decode_cmd(merged_file, decoded_file)
     try:
         absolute_file_directory = os.path.dirname(os.path.abspath(__file__))
         os.chdir(absolute_file_directory)
+        # TODO: On the fence here about whether to use shell
+        # direction to create the output file, or to capture output &
+        # write to file ourselves.
         p3 = subprocess.Popen(decode_cmd,
                               shell=True,
                               cwd='../../utils/satnogs-decoders')
