ef59320f49a0756ed5cc0d2fa40a05c4d6663eaf
Red Boumghar <redouane.boumghar@free.fr>
2019-07-07 10:50:22 +0000

    wip: setup tentative with dependency links
    Dropped solution since now glouton is on pypi

--- setup.py
+++ setup.py
@@ -20,7 +20,14 @@ setup(
     url="https://gitlab.com/crespum/polaris",
     license="",
     author="",
-    install_requires=["pandas", "scikit-learn", "xgboost", "kaitaistruct"],
+    install_requires=["kaitaistruct", "glouton==0.1.0",
+                      "click", "tqdm",
+                      "pandas", "scikit-learn", "xgboost"],
+    dependency_links=[
+                      # "https://github.com/deckbsd/glouton-satnogs-data-downloader/tarball/master#egg=glouton",
+                      # "https://github.com/deckbsd/glouton-satnogs-data-downloader/releases/tag/0.1.0#egg=glouton-0.1.0"
+                      "https://github.com/deckbsd/glouton-satnogs-data-downloader/archive/0.1.0.zip#egg=glouton-0.1.0"
+                      ],
     python_requires='>=3',
     extras_require={"test": ["pytest"]},
     packages=find_packages(exclude=["tests", "docs"]),
