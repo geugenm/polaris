1e3acdb552eef31a2d64a7d5311bafa4c08f3400
Patrick Dohmen <dl4pd@darc.de>
2019-07-19 23:31:17 +0200

    Add descriptive comments
    Signed-off-by: Patrick Dohmen <dl4pd@darc.de>

--- tests/test_predictor_xcorr.py
+++ tests/test_predictor_xcorr.py
@@ -1,3 +1,7 @@
+"""
+`pytest` testing framework file for xcorr predictor
+"""
+
 import os
 import sys
 import pandas as pd
@@ -9,6 +13,10 @@ from polaris.learning.predictor.cross_correlation import XCorr
 
 
 def test_xcorr():
+    """
+    `pytest` entry point
+    """
+
     test_df = pd.DataFrame({
         "A":[4, 123, 24.2, 3.14, 1.41],
         "B":[7,   0, 24.2, 3.14, 8.2]
@@ -24,6 +32,10 @@ def test_xcorr():
     
 
 def test_xcorr_pipeline():
+    """
+    `pytest` entry point
+    """
+
     pipeline = Pipeline([
         ("deps", XCorr())
     ])
