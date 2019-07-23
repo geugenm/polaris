1a3164b09b4c115e3df1d752f976fa0e77525e99
Patrick Dohmen <dl4pd@darc.de>
2019-07-23 15:04:09 +0200

    Introduce `versioneer`
    Enables proper versioning using "tags" in the GitLab-CI.
    Adding `versioneer to this project forces some changes to the projects'
    `setup.py` and `setup.cfg` files.
    
    Signed-off-by: Patrick Dohmen <dl4pd@darc.de>

--- setup.py
+++ setup.py
@@ -1,38 +1,10 @@
-""" polaris setuptools-based setup.
+from __future__ import absolute_import, division, print_function
 
-"""
+from setuptools import setup
 
-from codecs import open
-from os import path
-
-from setuptools import find_packages, setup
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+import versioneer
 
 setup(
-    version="0.1.2",
-    name="polaris",
-    description="",
-    long_description=long_description,
-    url="https://gitlab.com/crespum/polaris",
-    license="",
-    author="",
-    install_requires=[
-        "kaitaistruct", "glouton", "click", "tqdm", "pandas", "scikit-learn",
-        "xgboost", "satnogsdecoders"
-    ],
-    python_requires='>=3',
-    extras_require={"test": ["pytest"]},
-    packages=find_packages(exclude=["tests", "docs"]),
-    scripts=["bin/polaris"],
-    keywords="telemetry, satellite, machine learning",
-    classifiers=[
-        "Development Status :: 3 - Alpha", "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: LGPL"
-    ],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
 )
