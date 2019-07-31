3a2a785d496bbf28f00e779e0be96d3376ea196f
Aditya Malshikhare <malshikhareaditya@gmail.com>
2019-07-31 09:33:59 +0530

    Add feature extraction script for polaris learning

--- polaris/polaris.py
+++ polaris/polaris.py
@@ -7,6 +7,19 @@ import click
 
 from polaris import __version__
 from polaris.data_fetch.data_fetch_decoder import data_fetch_decode
+from polaris.learning.feature import best_transformed_features
+
+import ast
+
+
+class PythonLiteralOption(click.Option):
+
+    def type_cast_value(self, ctx, value):
+        try:
+            return ast.literal_eval(value)
+        except Exception:
+            raise click.BadParameter(value)
+
 
 # Logger configuration
 LOGGER = logging.getLogger(__name__)
@@ -23,12 +36,26 @@ LOGGER.addHandler(CH)
 
 
 @click.version_option(version=__version__)
-@click.group()
+@click.group(chain=True)
 def cli():
     """
     Tool for analyzing satellite telemetry
     """
     return
+
+# TODO: Uncomment these imports when we're ready to start using them
+# import data_viz
+# import learning
+
+
+# Logger configuration
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+formatter = logging.Formatter(log_format)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
 
 
 @click.command('fetch',
@@ -63,6 +90,27 @@ def cli_learning():
     """
     LOGGER.debug('[FIXME] Learning goes here')
     # learning()
+
+
+@cli_learning.command('feature_extraction',
+                      short_help='Feature extraction for the polaris')
+@cli_learning.argument('path_to_csv',
+                       required=True,
+                       type=click.Path(exists=True, resolve_path=True))
+@cli_learning.option('--transformer', '-tf', cls=PythonLiteralOption,
+                     default=[])
+@cli_learning.option('--start_date',
+                     '-s',
+                     is_flag=False,
+                     help='Start date of the time period for analysis.')
+@cli_learning.option('--end_date',
+                     '-e',
+                     is_flag=False,
+                     help='End date of the time period for analysis.')
+@cli_learning.pass_context
+def cli_feature(ctx, start_date, end_date, path_to_csv, transformer):
+    logger.info("Input CSV file is " + path_to_csv)
+    best_transformed_features(path_to_csv, transformer, start_date, end_date)
 
 
 @click.command('viz', short_help='data-viz help')
