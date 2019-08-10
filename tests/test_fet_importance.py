1e85dd1e22679425cf07d21221b226eaaa7adf4d
Aditya Malshikhare <malshikhareaditya@gmail.com>
2019-08-10 19:36:55 +0530

    Add tests for get_time_lag function

---
+++ tests/test_fet_importance.py
@@ -0,0 +1,13 @@
+import polaris.learning.feature.extraction as ext
+import unittest
+
+
+class test_feature_extraction(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_get_time_lags(self):
+        self.assertEqual(ext.get_time_lag('TSIntegrale("30min")'), "30min")
+
+    if __name__ == '__main__':
+        unittest.main(argv=['first-arg-is-ignored'], exit=False)
