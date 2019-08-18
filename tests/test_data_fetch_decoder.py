dc54aead6c59dbb7f191c4e0ee843ff224a68ca9
Xabier Crespo Álvarez <xabicrespog@gmail.com>
2019-08-18 16:16:18 +0200

    fetch: add tests for date builder

--- tests/test_data_fetch_decoder.py
+++ tests/test_data_fetch_decoder.py
@@ -2,6 +2,8 @@
 pytest testing framework for data_fetch module
 """
 
+import datetime
+
 import pytest
 from polaris.data_fetch import data_fetch_decoder
 
@@ -29,3 +31,21 @@ def test_find_satellite_no_decoder(satellite_list):
     test_satellite = 'NoDecoderSatellite'
     with pytest.raises(data_fetch_decoder.NoDecoderForSatellite):
         _ = data_fetch_decoder.find_satellite(test_satellite, satellite_list)
+
+
+def test_build_dates_from_string():
+    """Test dates conversion for build_start_and_end_dates()
+    """
+    start_date_str = '2019-08-14'
+    end_date_str = '2019-08-16'
+    start_date, end_date = data_fetch_decoder.build_start_and_end_dates(
+        start_date_str, end_date_str)
+    assert end_date - start_date == datetime.timedelta(days=2)
+
+
+def test_build_dates_from_default():
+    """Test default dates generation for build_start_and_end_dates()
+    """
+    start_date, end_date = data_fetch_decoder.build_start_and_end_dates(
+        None, None)
+    assert end_date - start_date == datetime.timedelta(seconds=3600)
