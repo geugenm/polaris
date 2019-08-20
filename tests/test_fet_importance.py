d4a901ad48e9fc7f30939110bbc01127ed38211e
aditya <malshikhareaditya@gmail.com>
2019-08-20 22:03:34 +0530

    Add docstrings and fix function name in test_fet_importance.py

--- tests/test_fet_importance.py
+++ tests/test_fet_importance.py
@@ -1,13 +1,20 @@
+"""
+This is a module to test extraction.py
+
+"""
 import unittest
 
 import polaris.learning.feature.extraction as ext
 
 
-class test_feature_extraction(unittest.TestCase):
+class TestFeatureExtraction(unittest.TestCase):
+    """
+    Class for testing feature extraction.
+    """
     def setUp(self):
         pass
 
-    def test_get_time_lags(self):
+    def _test_get_time_lags(self):
         self.assertEqual(ext.get_time_lag('TSIntegrale("30min")'), "30min")
 
     if __name__ == '__main__':
