87437c1684be81f80bdeb8ee461de1d07795ec15
eugene <glebajk@gmail.com>
2024-05-09 12:51:20 +0300

    Removed versioneer, tests and other stuff, to make the arch simplier

--- setup.py
+++ setup.py
@@ -2,9 +2,4 @@ from __future__ import absolute_import, division, print_function
 
 from setuptools import setup
 
-import versioneer
-
-setup(
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-)
+setup()
